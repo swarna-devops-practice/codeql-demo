28e5faf2b9bdfe09104a21d9f34724aa7786c4ee
SwarnaAnjani <swarnanjanidevershetty23@gmail.com>
2026-08-22 18:16:53 -0500

    add vulnerablity

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const { exec } = require("child_process");
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("GitHub Security Demo");
+});
+
+app.get("/run", (req, res) => {
+    const command = req.query.command;
+
+    exec(command, (error, stdout) => {
+        if (error) {
+            res.send(error.message);
+            return;
+        }
+
+        res.send(stdout);
+    });
 });
 
 app.listen(3000);
